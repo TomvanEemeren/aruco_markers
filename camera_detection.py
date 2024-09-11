3b950d4a7959180f4e3d1a2e6113ed1c472153f7
Tom van Eemeren <t.v.eemeren@student.tue.nl>
2024-09-11 16:50:13 +0200

    Add aruco markers example

---
+++ camera_detection.py
@@ -0,0 +1,122 @@
+import sys
+import cv2
+import argparse
+import numpy as np
+
+# Allow parsing arguments via the command line
+parser = argparse.ArgumentParser()
+parser.add_argument("-t", "--type", type=str, metavar='', default="DICT_6X6_250",
+                    help="ArUco dictionary to use (default: DICT_6X6_250)")
+
+ARUCO_DICT = {
+  "DICT_4X4_50": cv2.aruco.DICT_4X4_50,
+  "DICT_4X4_100": cv2.aruco.DICT_4X4_100,
+  "DICT_4X4_250": cv2.aruco.DICT_4X4_250,
+  "DICT_4X4_1000": cv2.aruco.DICT_4X4_1000,
+  "DICT_5X5_50": cv2.aruco.DICT_5X5_50,
+  "DICT_5X5_100": cv2.aruco.DICT_5X5_100,
+  "DICT_5X5_250": cv2.aruco.DICT_5X5_250,
+  "DICT_5X5_1000": cv2.aruco.DICT_5X5_1000,
+  "DICT_6X6_50": cv2.aruco.DICT_6X6_50,
+  "DICT_6X6_100": cv2.aruco.DICT_6X6_100,
+  "DICT_6X6_250": cv2.aruco.DICT_6X6_250,
+  "DICT_6X6_1000": cv2.aruco.DICT_6X6_1000,
+  "DICT_7X7_50": cv2.aruco.DICT_7X7_50,
+  "DICT_7X7_100": cv2.aruco.DICT_7X7_100,
+  "DICT_7X7_250": cv2.aruco.DICT_7X7_250,
+  "DICT_7X7_1000": cv2.aruco.DICT_7X7_1000,
+  "DICT_ARUCO_ORIGINAL": cv2.aruco.DICT_ARUCO_ORIGINAL
+}
+
+def acuro_display(corners, ids, rejected, frame):
+    """
+    Draws a bounding box around the detected ArUco markers, calculates and 
+    draws the center of the markers,and displays the marker IDs on the frame.
+
+    Parameters:
+    - corners (list): List of detected ArUco marker corners.
+    - ids (ndarray): Array of detected ArUco marker IDs.
+    - rejected (list): Returned list of ArUco marker candidates.
+    - frame (ndarray): Input frame to display the markers on.
+
+    Returns:
+    - frame (ndarray): Frame with ArUco markers displayed.
+    """
+
+    # Check that at least one ArUco marker was detected
+    if len(corners) > 0:
+
+        ids = ids.flatten()
+
+        # Loop over the detected ArUco corners
+        for (marker_corner, marker_id) in zip(corners, ids):
+        
+            # Extract the marker corners
+            corners = marker_corner.reshape((4, 2))
+            (top_left, top_right, bottom_right, bottom_left) = corners
+
+            # Convert the (x,y) coordinate pairs to integers
+            top_right = (int(top_right[0]), int(top_right[1]))
+            bottom_right = (int(bottom_right[0]), int(bottom_right[1]))
+            bottom_left = (int(bottom_left[0]), int(bottom_left[1]))
+            top_left = (int(top_left[0]), int(top_left[1]))
+
+            # Draw the bounding box of the ArUco detection
+            cv2.line(frame, top_left, top_right, (0, 255, 0), 2)
+            cv2.line(frame, top_right, bottom_right, (0, 255, 0), 2)
+            cv2.line(frame, bottom_right, bottom_left, (0, 255, 0), 2)
+            cv2.line(frame, bottom_left, top_left, (0, 255, 0), 2)
+
+            # Calculate and draw the center of the ArUco marker
+            center_x = int((top_left[0] + bottom_right[0]) / 2.0)
+            center_y = int((top_left[1] + bottom_right[1]) / 2.0)
+            cv2.circle(frame, (center_x, center_y), 4, (0, 0, 255), -1)
+
+            # Draw the ArUco marker ID on the video frame
+            # The ID is always located at the top_left of the ArUco marker
+            cv2.putText(frame, str(marker_id), 
+                (top_left[0], top_left[1] - 15),
+                cv2.FONT_HERSHEY_SIMPLEX,
+                0.5, (0, 255, 0), 2)
+
+    return frame
+
+def main():
+    args = vars(parser.parse_args())
+    aruco_type = args["type"]
+
+    # Check that we have a valid ArUco marker
+    if ARUCO_DICT.get(aruco_type) is None:
+        print("[INFO] ArUCo tag of '{}' is not supported".format(args["type"]))
+        sys.exit(0)
+
+    # Load the ArUco dictionary
+    print("[INFO] detecting '{}' markers...".format(aruco_type))
+    aruco_dictionary = cv2.aruco.Dictionary_get(ARUCO_DICT[aruco_type])
+    aruco_parameters = cv2.aruco.DetectorParameters_create()
+
+    # Start the video stream
+    cap = cv2.VideoCapture(0)
+
+    while True:
+        ret, frame = cap.read()  
+
+        # Detect ArUco markers in the video frame
+        (corners, ids, rejected) = cv2.aruco.detectMarkers(
+            frame, aruco_dictionary, parameters=aruco_parameters)
+
+        # Display the resulting frame
+        # For raspberry pi camera, this drastically reduces performance
+        detected_markers = acuro_display(corners, ids, rejected, frame)
+        cv2.imshow('frame', detected_markers)
+
+        # If "q" is pressed on the keyboard, exit this loop
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+           break
+
+    # Close down the video stream
+    cap.release()
+    cv2.destroyAllWindows()
+   
+if __name__ == '__main__':
+  main()
